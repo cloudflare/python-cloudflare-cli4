630612a52adad05fc9fc1ec05c52a9c08eacaa80
Patryk Szczygłowski <patryk@patryk.net>
2019-05-14 09:25:39 -0700

    Add Python 3.7

--- setup.py
+++ setup.py
@@ -52,7 +52,8 @@ def main():
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6'
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7'
         ]
     )
 
