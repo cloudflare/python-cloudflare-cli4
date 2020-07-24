80918ea248c5c2cf3f9013ca762eb844d898de1e
Martin Levy <martin@cloudflare.com>
2020-07-24 11:11:00 -0700

    now with curl style debug - i.e. matches api information page

--- CloudFlare/utils.py
+++ CloudFlare/utils.py
@@ -33,3 +33,38 @@ def sanitize_secrets(secrets):
         secrets_copy['Authorization'] = redacted_phrase
 
     return secrets_copy
+
+def build_curl(method, url, headers, params, data, files):
+    """ misc utilities  for Cloudflare API"""
+
+    msg = []
+    # url
+    url_full = url
+    if params is not None:
+        for k in params:
+            if k is None:
+                continue
+            url_full += '&%s=%s' % (k, params[k])
+        url_full = url_full.replace('&', '?', 1)
+    msg.append('       curl -X %s "%s" \\' % (str(method), str(url_full)))
+    # headers
+    h = sanitize_secrets(headers)
+    for k in h:
+        if k is None:
+            continue
+        msg.append('            -H "%s: %s" \\' % (k, h[k]))
+    # data
+    if data is not None:
+        try:
+            str_data = json.dumps(data)
+        except:
+            str_data = str(data)
+        msg.append('            --data \'%s\' \\' % (str_data.replace('\n', ' ')))
+    # files
+    if files is not None:
+        msg.append('            --form "file=@%s" \\' % (files))
+
+    # remove the last \ from the last line.
+    msg[-1] = msg[-1][:-1]
+
+    return '\n'.join(msg)
