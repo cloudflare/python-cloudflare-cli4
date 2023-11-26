3621a4d18919f65a8f5b8979e76d3a0bd453fd12
Martin Levy <mahtin@mahtin.com>
2023-11-25 19:26:44 -0800

    clean up and more

--- examples/example_ai_translate.py
+++ examples/example_ai_translate.py
@@ -4,13 +4,13 @@ import os
 import sys
 import CloudFlare
 
-def translate_call(cf):
+def find_call(cf, verbs):
     # So we walk over the @ via a getattr() call.
     # We also have to deal with a . in a verb - that does not work in Python. So sad.
     # Also, the - is actually an _ in this Python library. 
     # This is not normally needed for other calls
     m = cf
-    for verb in 'accounts/ai/run/@cf/meta/m2m100_1.2b'.split('/'):
+    for verb in verbs.split('/'):
         m = getattr(m, verb)
     return m
 
@@ -19,7 +19,8 @@ def doit(account_name, english_text):
     # Or place these in your cloudflare.cfg file
     os.environ['CLOUDFLARE_API_EXTRAS'] = '/accounts/:id/ai/run/@cf/meta/m2m100-1.2b'
 
-    cf = CloudFlare.CloudFlare()
+    # We set the timeout because these AI calls take longer than normal API calls
+    cf = CloudFlare.CloudFlare(global_request_timeout=120)
 
     try:
         params = {'name': account_name, 'per_page': 1}
@@ -35,13 +36,11 @@ def doit(account_name, english_text):
         'target_lang': 'french',
     }
 
-    # This should be easy to call; however, the @ will not work in Python (or many languages)
-    # r = cf.accounts.ai.run.@cf.meta.m2m100-1.2b(account_id, data=translate_data)
-
-    the_call = translate_call(cf)
-
     try:
-        r = the_call.post(account_id, data=translate_data)
+        # This should be easy to call; however, the @ will not work in Python (or many languages)
+        # r = cf.accounts.ai.run.@cf.meta.m2m100-1.2b(account_id, data=translate_data)
+        # We find the endpoint via a quick string search
+        r = find_call(cf, 'accounts/ai/run/@cf/meta/m2m100_1.2b').post(account_id, data=translate_data)
     except CloudFlare.exceptions.CloudFlareAPIError as e:
         exit('/ai.run %d %s - api call failed' % (e, e))
 
