61ca2d6ee7846403f892ea5a763ca67d9ae37458
Martin Levy <mahtin@mahtin.com>
2023-12-26 11:09:14 -0800

    automate the list of aliases - far better for future

--- tests/test_radar_returning_csv.py
+++ tests/test_radar_returning_csv.py
@@ -4,6 +4,7 @@ import os
 import sys
 import uuid
 
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 import CloudFlare
 
@@ -16,17 +17,30 @@ def test_cloudflare():
     cf = CloudFlare.CloudFlare()
     assert isinstance(cf, CloudFlare.CloudFlare)
 
-def test_radar_datasets_ranking_top_200():
-    results = cf.radar.datasets('ranking_top_200')
-    assert len(results) > 0
-    lines = results.splitlines()
-    assert lines[0] == 'domain'
-    assert len(lines) >= 200+1
+aliases = None
 
-def test_radar_datasets_ranking_top_1000():
-    results = cf.radar.datasets('ranking_top_1000')
+def test_radar_datasets_ranking():
+    # get the list of aliases - we only need to grab 12 values
+    global aliases
+    params = {'limit':12}
+    results = cf.radar.datasets(params=params)
     assert len(results) > 0
-    lines = results.splitlines()
-    assert lines[0] == 'domain'
-    assert len(lines) >= 1000+1
+    assert 'datasets' in results
+    aliases = []
+    for v in results['datasets']:
+        aliases.append(
+            (v['id'], v['alias'], v['meta']['top'])
+        )
+    aliases = sorted(aliases, key=lambda v: v[2], reverse=False)
+
+def test_radar_datasets_ranking_three_aliases():
+    for v in aliases[0:3]:
+        alias = v[1]
+        n_lines = v[2]
+        results = cf.radar.datasets(alias)
+        # produces CSV results
+        assert len(results) > 0
+        lines = results.splitlines()
+        assert lines[0] == 'domain'
+        assert len(lines) >= n_lines + 1
 
