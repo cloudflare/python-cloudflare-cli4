fc74b5ac5e5085763dae72e4a45641912e02b1b4
Martin Levy <mahtin@mahtin.com>
2023-12-25 16:17:21 -0800

    more test cases

--- tests/test_ips.py
+++ tests/test_ips.py
@@ -6,15 +6,32 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import CloudFlare
 
-class TestCloudflare:
-    def test_ips(self):
-        # no auth required
-        cf = CloudFlare.CloudFlare()
-        assert isinstance(cf, CloudFlare.CloudFlare)
-        ips = cf.ips()
-        assert isinstance(ips, dict)
-        assert isinstance(ips['ipv4_cidrs'], list)
-        assert isinstance(ips['ipv6_cidrs'], list)
-        assert len(ips['ipv4_cidrs']) > 0
-        assert len(ips['ipv6_cidrs']) > 0
+cf = None
 
+def test_cloudflare():
+    global cf
+    cf = CloudFlare.CloudFlare()
+    assert isinstance(cf, CloudFlare.CloudFlare)
+
+def test_ips():
+    # no auth required
+    cf = CloudFlare.CloudFlare()
+    assert isinstance(cf, CloudFlare.CloudFlare)
+    ips = cf.ips()
+    assert isinstance(ips, dict)
+    assert isinstance(ips['ipv4_cidrs'], list)
+    assert isinstance(ips['ipv6_cidrs'], list)
+    assert len(ips['ipv4_cidrs']) > 0
+    assert len(ips['ipv6_cidrs']) > 0
+
+def test_ips_plus_jdcloud():
+    # no auth required
+    params = {'networks':'jdcloud'}
+    ips = cf.ips(params=params)
+    assert isinstance(ips, dict)
+    assert isinstance(ips['ipv4_cidrs'], list)
+    assert isinstance(ips['ipv6_cidrs'], list)
+    assert isinstance(ips['jdcloud_cidrs'], list)
+    assert len(ips['ipv4_cidrs']) > 0
+    assert len(ips['ipv6_cidrs']) > 0
+    assert len(ips['jdcloud_cidrs']) > 0
