562268622c679e261b405838708e93f3d78e3b0c
Martin Levy <mahtin@mahtin.com>
2023-12-31 09:33:59 -0800

    tests more return values

--- CloudFlare/tests/test_api_dump.py
+++ CloudFlare/tests/test_api_dump.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import re
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
@@ -18,17 +19,49 @@ def test_cloudflare(debug=False):
     cf = CloudFlare.CloudFlare(debug=debug)
     assert isinstance(cf, CloudFlare.CloudFlare)
 
-def test_dump_commands():
-    """dump a tree of all the known API commands"""
-    w = cf.api_list()
-    assert len(w) > 0
+verb_only = re.compile('^[a-zA-Z0-9][a-zA-Z0-9_\-]*[a-zA-Z0-9]$')
 
-def test_dump_commands_from_web():
+def check_cmd_syntax(cmd):
+    assert '/' == cmd[0]
+    for verb in cmd[1:].split('/'):
+        if verb[0] == '@':
+            # don't want to check the rest of the api - it's an AI one
+            break
+        if verb[0] == ':':
+            # :id or equiv
+            assert bool(verb_only.match(verb[1:]))
+        else:
+            # just a verb
+            assert bool(verb_only.match(verb))
+
+def check_method_syntax(method):
+    assert method in ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+
+def test_api_list():
+    """dump a tree of all the known API commands"""
+    api_list = cf.api_list()
+    assert len(api_list) > 0
+    for api in api_list:
+        check_cmd_syntax(api)
+
+def test_api_from_openapi():
     """dump a tree of all the known API commands - from web"""
-    w = cf.api_from_openapi(OPENAPI_URL)
-    assert len(w) > 0
+    api_list = cf.api_from_openapi()
+    assert len(api_list) > 0
+    for api in api_list:
+        # {'action': 'GET', 'cmd': '/accounts', 'deprecated': ...
+        assert 'action' in api
+        assert 'cmd' in api
+        check_method_syntax(api['action'])
+        check_cmd_syntax(api['cmd'])
+
+def test_api_from_openapi_with_url():
+    """dump a tree of all the known API commands - from web"""
+    api_list = cf.api_from_openapi(OPENAPI_URL)
+    assert len(api_list) > 0
 
 if __name__ == '__main__':
     test_cloudflare(debug=True)
-    test_dump_commands()
-    test_dump_commands_from_web()
+    test_api_list()
+    test_api_from_openapi()
+    test_api_from_openapi_with_url()
