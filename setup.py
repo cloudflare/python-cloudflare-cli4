c2c4adf2d4f75ee76bb25d82ac8628945644bd98
Martin Levy <mahtin@mahtin.com>
2024-02-22 09:32:55 +0900

    solidfied the Python3 only mindset

--- setup.py
+++ setup.py
@@ -43,7 +43,16 @@ def main():
             'Intended Audience :: Developers',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
             'Programming Language :: Python :: 3'
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
+            'Programming Language :: Python :: 3 :: Only',
         ]
     )
 
